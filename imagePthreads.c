abbbcef55ef9351d956d94f4916cd7f300a0f36c
James Cooper <jamesco@udel.edu>
2021-05-09 15:02:10 -0400

    commit

--- imagePthreads.c
+++ imagePthreads.c
@@ -67,8 +67,8 @@ void *convoluteThread(void* rank){
     long pix_row = srcImage->height;
     long pix_p = srcImage->width;
 	
-    long my_first_row = (my_rank  * pix_row);
-    long my_last_row = ((my_rank + 1) *  pix_row); 
+    long my_first_row = (my_rank  * pix_row) / N;
+    long my_last_row = ((my_rank + 1) *  pix_row) / N ; 
     long my_first_pix = (my_rank * pix_p) ;
     long my_last_pix = ((my_rank + 1) * pix_p) ;
 
