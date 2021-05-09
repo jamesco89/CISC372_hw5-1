6278bb04459f0db3a1ea3369c41749431eb2525e
James Cooper <jamesco@udel.edu>
2021-05-08 22:30:42 -0400

    commit

--- imagePthreads.c
+++ imagePthreads.c
@@ -15,7 +15,7 @@
 Image* srcImage;
 Image* destImage;
 enum KernelTypes type;
-const int N = 3000;
+const int N = 4000;
 
 //An array of kernel matrices to be used for image convolution.  
 //The indexes of these match the enumeration from the header file. ie. algorithms[BLUR] returns the kernel corresponding to a box blur.
