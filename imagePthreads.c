4cb7088c8dd055e7b21f687cc27ff3fa5c419686
James Cooper <jamesco@udel.edu>
2021-05-09 14:28:23 -0400

    commit

--- imagePthreads.c
+++ imagePthreads.c
@@ -15,7 +15,7 @@
 Image* srcImage;
 Image* destImage;
 enum KernelTypes type;
-const int N = 200;
+const int N = 1000;
 
 //An array of kernel matrices to be used for image convolution.  
 //The indexes of these match the enumeration from the header file. ie. algorithms[BLUR] returns the kernel corresponding to a box blur.
@@ -67,7 +67,7 @@ void *convoluteThread(void* rank){
     long pix_row = srcImage->height;
     long pix_p = srcImage->width;
 	
-    long my_first_row = (my_rank  * pix_row) / N;
+    long my_first_row = (my_rank  * pix_row) / N ;
     long my_last_row = ((my_rank + 1) *  pix_row) / N; 
     long my_first_pix = (my_rank * pix_p) / N;
     long my_last_pix = ((my_rank + 1) * pix_p) / N;
